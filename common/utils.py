63d70b64d05403534f0f76ca9e5cf4be6058bc08
oucyz <dks.9886.po@gmail.com>
2021-08-12 23:45:42 +0900

    add preprocess in utils

--- common/utils.py
+++ common/utils.py
@@ -14,3 +14,19 @@ def clip_grads(grads, max_norm):
     if rate < 1:
         for grad in grads:
             grad *= rate
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split()
+
+    id_to_word = {}
+    word_to_id = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = [word_to_id[word] for word in words]
+    return corpus, word_to_id, id_to_word
