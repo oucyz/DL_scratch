a58e31173bd2ae5c89365cba609ab86ddd21f017
oucyz <dks.9886.po@gmail.com>
2021-08-12 15:00:11 +0900

    add int_to_onehot

--- common/functions.py
+++ common/functions.py
@@ -16,3 +16,14 @@ def cross_entropy_error(y_hat, y):
     delta = 1e-7
     batch_size = y_hat.shape[1]
     return -np.sum(y * np.log(y_hat + delta)) / batch_size
+
+def int_to_onehot(labels):
+    # ラベルの最大値 + 1 を取得
+    out_size = np.max(labels) + 1
+    # out_size += 1
+    # print(out_size)
+
+    onehot_labels = np.zeros((out_size, labels.shape[0]))
+    for i in range(len(labels)):
+        onehot_labels[:, i] = np.eye(out_size)[labels[i]]
+    return onehot_labels
