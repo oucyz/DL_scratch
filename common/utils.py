06d30967697cb455da457e065cec9dbfe2bf6bc9
oucyz <dks.9886.po@gmail.com>
2021-08-13 21:56:10 +0900

    add cos_similarity

--- common/utils.py
+++ common/utils.py
@@ -51,3 +51,9 @@ def create_co_matrix(corpus, vocab_size, window_size=1):
                 co_matrix[word_id, right_word_id] += 1
     return co_matrix
 
+
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / (np.sqrt(np.sum(np.power(x, 2))) + eps)
+    ny = y / (np.sqrt(np.sum(np.power(y, 2))) + eps)
+    return np.dot(nx, ny)
+
